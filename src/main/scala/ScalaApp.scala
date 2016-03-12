8166c9015e7f461cea81194c2d0c04a1a1a03ba2
tscheys <tim.scheys@gmail.com>
2016-03-12 15:50:49 +0100

    adjust comments in main

--- src/main/scala/ScalaApp.scala
+++ src/main/scala/ScalaApp.scala
@@ -1,27 +1,16 @@
-/* SampleApp.scala:
-   This application simply counts the number of lines that contain "val" from itself
- */
+// import dependencies 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql
 
- 
+// main class  
 object ScalaApp {
   def main(args: Array[String]) {
-    //val txtFile = "/Users/tscheys/ScalaApp/src/main/scala/ScalaApp.scala"
-    
     val conf = new SparkConf().setAppName("Sample Application").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     val df = sqlContext.read.json("/Users/tscheys/second.json")
     df.show()
-    
-    /* val txtFileLines = sc.textFile(txtFile , 2).cache()
-     * val numAs = txtFileLines .filter(line => line.contains("val")).count()
-     * 
-     */
-    
-    println("Het werkt")
   }
 }
