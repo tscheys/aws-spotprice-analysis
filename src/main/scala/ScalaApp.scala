46abbf0415802c29f97fbebc9c764797be85558c
tscheys <tim.scheys@gmail.com>
2016-03-15 09:29:40 +0100

    convert spotprice to double

--- src/main/scala/ScalaApp.scala
+++ src/main/scala/ScalaApp.scala
@@ -1,16 +1,28 @@
-// import dependencies 
+// import dependencies
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql
+import org.apache.spark.sql.functions._
 
-// main class  
+// main class
 object ScalaApp {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Sample Application").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val df = sqlContext.read.json("/Users/tscheys/second.json")
+    val df = sqlContext.read.json("/Users/tscheys/ScalaApp/aws.json")
+
+    // inspect data
     df.show()
+    df.printSchema()
+
+    // change data types
+    val toDouble = udf[Double, String]( _.toDouble)
+    val featureDf = df
+      .withColumn("SpotPrice", toDouble(df("SpotPrice")))
+      .select("AvailabilityZone","InstanceType","SpotPrice")
+    featureDf.printSchema()
+
   }
 }
