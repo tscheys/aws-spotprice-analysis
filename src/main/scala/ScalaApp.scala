06ff55ab5c542b1c65ce53bd7c4105f9524c49da
tscheys <tim.scheys@gmail.com>
2016-04-09 18:24:48 +0200

    make csv overwrite old basetable

--- src/main/scala/ScalaApp.scala
+++ src/main/scala/ScalaApp.scala
@@ -2,18 +2,12 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.hive
+
 import org.apache.spark.sql.hive._
+import org.apache.spark.sql.SaveMode
 
 // import jodatime
 import com.github.nscala_time.time.Imports._
-import org.apache.spark.rdd
-import org.apache.spark.rdd._
-// ml deps
-import org.apache.spark.ml.feature.{StringIndexer, IndexToString, VectorIndexer}
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.feature.Binarizer
-import org.apache.spark.ml.feature.VectorIndexer
 
 // main class
 object ScalaApp {
@@ -24,7 +18,6 @@ object ScalaApp {
     var df = sqlContext.read.json("/Users/tscheys/ScalaApp/aws.json")
 
     // CONSTANTS
-
     // prices for on demand instances
     val M1_EU_US = 0.095
     val C3_EU_US = 0.12
@@ -42,14 +35,9 @@ object ScalaApp {
       if(dt.getDayOfWeek < 6) {1} else {0}
     })
 
-    // get day of week
-    def dayOfWeek = udf((date: String) => {
-      val fmt = DateTimeFormat.forPattern("yyyy-MM-dd")
-      val dt = fmt.parseDateTime(date)
-      dt.getDayOfWeek
-    })
+    def aggregate(split: Int) = udf((date:String, hours: Int, minutes: Int) => {
+      // aggregate datapoints on 15, 30 or 60 minute intervals
 
-    def combine(split: Int) = udf((date:String, hours: Int, minutes: Int) => {
       // initialize variable to be re-assigned a value in pattern matching
       var group = 0
 
@@ -69,14 +57,12 @@ object ScalaApp {
         case 60 => group = 1
       }
 
-      // create string ready for unix_timestamp conversion
+      // create string ready for unix_TimeStamp conversion
       // minutes in this unix are meaningless, since we use the group variable to perform a .groupby().mean("spotPrice") on the aggregation column
       date + " " + hours + ":" + group + ":" + "00"
 
     })
     def isIrrational = udf((zone: String, instance: String, price: Double) => {
-    // TODO: rewrite this heaping pile of shit
-
       // remove subregion reference a, b, c
       val region  = zone.dropRight(1)
 
@@ -98,37 +84,33 @@ object ScalaApp {
     // apply correct types to columns
     // rename other columns to camelCase
     df = df
-      .withColumn("spotPrice", col("SpotPrice").cast("Double"))
-      .withColumnRenamed("AvailabilityZone", "availabilityZone")
-      .withColumnRenamed("InstanceType", "instanceType")
-      .withColumnRenamed("ProductDescription", "productDescription")
-      .withColumnRenamed("Timestamp", "timeStamp")
+      .withColumn("SpotPrice", col("SpotPrice").cast("Double"))
 
     // create time variables
     df = df
-      .withColumn("date", substring(col("timeStamp"), 0, 10))
-      .withColumn("hours", substring(col("timeStamp"), 12,2).cast("Int"))
-      .withColumn("minutes", substring(col("timeStamp"), 15,2).cast("Int"))
+      .withColumn("date", substring(col("TimeStamp"), 0, 10))
+      .withColumn("hours", substring(col("TimeStamp"), 12,2).cast("Int"))
+      .withColumn("minutes", substring(col("TimeStamp"), 15,2).cast("Int"))
 
     // aggregate data (interpolation)
 
-    df = df.withColumn("aggregation", unix_timestamp(combine(15)(col("date"), col("hours"), col("minutes"))))
+    df = df.withColumn("aggregation", unix_timestamp(aggregate(15)(col("date"), col("hours"), col("minutes"))))
 
     // do quick check if aggregation is properly able to form groups
-    df.orderBy("availabilityZone", "instanceType", "aggregation").show()
+    df.orderBy("AvailabilityZone", "InstanceType", "aggregation").show()
 
-    // take mean over fixed time interval chosen in combine() function
+    // take mean over fixed time interval chosen in aggregate() function
     df = df
-      .groupBy("availabilityZone", "instanceType","aggregation").mean("spotPrice").sort("availabilityZone", "instanceType", "aggregation")
+      .groupBy("AvailabilityZone", "InstanceType","aggregation").mean("spotPrice").sort("AvailabilityZone", "InstanceType", "aggregation")
     df = df
       .withColumnRenamed("avg(spotPrice)", "spotPrice")
 
     // create separate time variables
     df = df
-      .withColumn("timestamp", from_unixtime(col("aggregation")))
-      .withColumn("hours", substring(col("timestamp"), 12, 2).cast("Int"))
-      .withColumn("quarter", substring(col("timestamp"), 16, 1).cast("Int"))
-      .withColumn("date", substring(col("timestamp"), 1, 10))
+      .withColumn("TimeStamp", from_unixtime(col("aggregation")))
+      .withColumn("hours", substring(col("TimeStamp"), 12, 2).cast("Int"))
+      .withColumn("quarter", substring(col("TimeStamp"), 16, 1).cast("Int"))
+      .withColumn("date", substring(col("TimeStamp"), 1, 10))
     df.show()
 
     // create aggregation variable (average spot price over every 15 minutes)
@@ -138,7 +120,7 @@ object ScalaApp {
       .withColumn("isDaytime", (col("hours") > 6 || col("hours") < 18).cast("Boolean"))
 
     df = df
-      .withColumn("isIrrational", isIrrational(col("availabilityZone"), col("instanceType"), col("spotPrice")).cast("Integer"))
+      .withColumn("isIrrational", isIrrational(col("AvailabilityZone"), col("InstanceType"), col("spotPrice")).cast("Integer"))
     // check frequency of irrational behaviour
     df.printSchema()
     df.show()
@@ -154,9 +136,9 @@ object ScalaApp {
     df.registerTempTable("cleanData")
     // use Spark window function to lag()
     df = sqlContext.sql("""SELECT a.*,
-        lag(a.spotPrice) OVER (PARTITION BY a.availabilityZone, a.instanceType ORDER BY a.aggregation) AS t1,
-        lag(a.spotPrice, 2) OVER (PARTITION BY a.availabilityZone, a.instanceType ORDER BY a.aggregation) AS t2,
-        lag(a.spotPrice, 3) OVER (PARTITION BY a.availabilityZone, a.instanceType ORDER BY a.aggregation) AS t3
+        lag(a.spotPrice) OVER (PARTITION BY a.AvailabilityZone, a.InstanceType ORDER BY a.aggregation) AS t1,
+        lag(a.spotPrice, 2) OVER (PARTITION BY a.AvailabilityZone, a.InstanceType ORDER BY a.aggregation) AS t2,
+        lag(a.spotPrice, 3) OVER (PARTITION BY a.AvailabilityZone, a.InstanceType ORDER BY a.aggregation) AS t3
         FROM cleanData a""")
 
     // some rows contain null values because we have shifted cols with window function
@@ -172,11 +154,11 @@ object ScalaApp {
       .withColumn("decreaseTemp", (col("priceChange") < 0).cast("Boolean"))
       .withColumn("sameTemp", (col("priceChange") === 0).cast("Boolean"))
 
-      df.registerTempTable("labelData")
-    df = sqlContext.sql("""SELECT a.*, lead(a.increaseTemp) OVER (PARTITION BY a.availabilityZone, a.instanceType ORDER BY a.aggregation) AS increase,
-      lead(a.decreaseTemp) OVER (PARTITION BY a.availabilityZone, a.instanceType ORDER BY a.aggregation) AS decrease,
-      lead(a.sameTemp) OVER (PARTITION BY a.availabilityZone, a.instanceType ORDER BY a.aggregation) AS same,
-      lead(a.spotPrice) OVER (PARTITION BY a.availabilityZone, a.instanceType ORDER BY a.aggregation) AS futurePrice
+    df.registerTempTable("labelData")
+    df = sqlContext.sql("""SELECT a.*, lead(a.increaseTemp) OVER (PARTITION BY a.AvailabilityZone, a.InstanceType ORDER BY a.aggregation) AS increase,
+      lead(a.decreaseTemp) OVER (PARTITION BY a.AvailabilityZone, a.InstanceType ORDER BY a.aggregation) AS decrease,
+      lead(a.sameTemp) OVER (PARTITION BY a.AvailabilityZone, a.InstanceType ORDER BY a.aggregation) AS same,
+      lead(a.spotPrice) OVER (PARTITION BY a.AvailabilityZone, a.InstanceType ORDER BY a.aggregation) AS futurePrice
       FROM labelData a""")
 
     // remove null rows created by performing a lead
@@ -185,7 +167,7 @@ object ScalaApp {
     df.show(400)
     df.printSchema()
 
-    var deviations = df.groupBy("availabilityZone", "instanceType", "date").agg(stddev("priceChange"))
+    var deviations = df.groupBy("AvailabilityZone", "InstanceType", "date").agg(stddev("priceChange"))
     deviations = deviations
       .withColumnRenamed("stddev_samp(priceChange,0,0)", "stddev")
 
@@ -197,7 +179,7 @@ object ScalaApp {
 
     // join deviations and df
     df = df
-      .join(deviations, Seq("availabilityZone", "instanceType", "date"))
+      .join(deviations, Seq("AvailabilityZone", "InstanceType", "date"))
 
     df = df
       .withColumn("isVolatile", col("priceChange") > (col("stddev") * 2))
@@ -206,11 +188,11 @@ object ScalaApp {
     df = df.na.fill(0.0, Seq("priceChange", "increase", "futurePrice", "isVolatile"))
 
     // check final basetable
-    df.orderBy("availabilityZone", "instanceType", "aggregation").show(400)
+    df.orderBy("AvailabilityZone", "InstanceType", "aggregation").show(400)
     df.printSchema()
 
     // save basetable to csv
-    df.write.format("com.databricks.spark.csv").option("header", "true").save("/Users/tscheys/thesis-data/basetable.csv")
+    df.write.format("com.databricks.spark.csv").option("header", "true").mode(SaveMode.Overwrite).save("/Users/tscheys/thesis-data/basetable.csv")
 
   }
 }
